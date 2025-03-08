65b0b469786046c8c94a6be9edfae3ab5ce51288
hadiel7 <0000124941507XSP@al.educacao.sp.gov.br>
2025-03-08 20:24:21 -0300

    Create rota.sql

---
+++ sql/04-queries/rota/rota.sql
@@ -0,0 +1,12 @@
+SELECT * FROM refood.rota; 
+
+/*Seleciona somente a coluna tipo_veiculo*/
+SELECT tipo_veiculo FROM refood.rota;
+
+/*Organiza a tabela origem de forma decresente, afetando o id também*/
+SELECT * FROM refood.rota ORDER BY origem DESC;
+
+/*Selecioa duas colunas e as muda para ordem decresente*/
+SELECT tipo_veiculo, placa_veiculo 
+FROM refood.rota 
+ORDER BY tipo_veiculo, placa_veiculo DESC;
