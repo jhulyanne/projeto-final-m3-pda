53b352a88c857e41ce16bc009bca8f51c439eaab
Stacy Amado <stacyamado17@gmail.com>
2025-03-11 16:33:42 -0300

    adicionando

---
+++ sql/04-queries/familia/familia.sql
@@ -0,0 +1,7 @@
+USE refood;
+
+SELECT * FROM familia WHERE quantidade_membros > 3;
+
+SELECT COUNT(*) AS total_familias FROM familia WHERE quantidade_membros > 3;
+
+SELECT quantidade_membros, COUNT(*) AS total_familias FROM familia GROUP BY quantidade_membros ORDER BY quantidade_membros;
