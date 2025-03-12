7e5c48d3df95fbbbe5507cfdaf07089241e19397
Stacy Amado <stacyamado17@gmail.com>
2025-03-12 18:20:39 -0300

    Mensagem do commit

--- sql/04-queries/familia/familia.sql
+++ sql/04-queries/familia/familia.sql
@@ -1,7 +1,9 @@
 USE refood;
 
-SELECT * FROM familia WHERE quantidade_membros > 3;
-
+SELECT nome_responsavel, quantidade_membros FROM refood.familia WHERE quantidade_membros > 3;
+ 
 SELECT COUNT(*) AS total_familias FROM familia WHERE quantidade_membros > 3;
 
 SELECT quantidade_membros, COUNT(*) AS total_familias FROM familia GROUP BY quantidade_membros ORDER BY quantidade_membros;
+
+FROM familia WHERE  > 3;
--- sql/04-queries/ponto-workshop/ponto-workshop.sql
+++ sql/04-queries/ponto-workshop/ponto-workshop.sql
@@ -4,4 +4,6 @@ SELECT COUNT(*) AS total_confirmados FROM refood.ponto_workshop WHERE status = '
 
 SELECT status, COUNT(*) AS total_por_status FROM refood.ponto_workshop GROUP BY status;
 
+SELECT DATE_FORMAT(data_e_hora, '%Y-%m') AS mes, COUNT(*) AS total_pedidos FROM ponto_workshop GROUP BY mes ORDER BY mes;
+
 
